3507132ea0393dc63cee0bbe7e6dd5773ab8ed84
Jennifer Bland <ratracegrad@gmail.com>
2017-03-11 21:33:36 -0500

    Added showMakes component

---
+++ components/showMakes/showMakes.js
@@ -0,0 +1,36 @@
+app.component('showMakes', {
+  templateUrl: 'components/showMakes/showMakes.html',
+  bindings: {
+    make: '='
+  },
+  controller: "MakePickerCtrl"
+});
+
+app.controller('MakePickerCtrl', function() {
+  this.makes = [
+    {
+      'key': 'Buick',
+      'value': 'Buick'
+    },
+    {
+      'key': 'Chevrolet',
+      'value': 'Chevrolet'
+    },
+    {
+      'key': 'Ford',
+      'value': 'Ford'
+    },
+    {
+      'key': 'Kia',
+      'value': 'Kia'
+    },
+    {
+      'key': 'Mazda',
+      'value': 'Mazda'
+    },
+    {
+      'key': 'Nissan',
+      'value': 'Nissan'
+    }
+  ];
+});
